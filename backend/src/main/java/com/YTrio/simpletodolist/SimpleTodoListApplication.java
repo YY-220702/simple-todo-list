d6a38104a274b98522785e32b893ca8b418ce266
Tomady <87797173+Tomady@users.noreply.github.com>
2022-07-19 04:15:17 +0900

    feat(security-config): JWT settings
    Add JWT for the security
    Add implementation 'org.springframework.boot:spring-boot-starter-security'
    Add testImplementation 'org.springframework.security:spring-security-test'
    Add implementation 'io.jsonwebtoken:jjwt-api:0.11.2'
    Add implementation 'io.jsonwebtoken:jjwt-impl:0.11.2'
    Add implementation 'io.jsonwebtoken:jjwt-jackson:0.11.2'

--- backend/src/main/java/com/YTrio/simpletodolist/SimpleTodoListApplication.java
+++ backend/src/main/java/com/YTrio/simpletodolist/SimpleTodoListApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleTodoListApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleTodoListApplication.class, args);
 	}
