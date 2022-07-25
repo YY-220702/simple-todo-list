eccfe3ccb32a9cf97eca37552f43a09841bf1113
hjyoon <se1620236@naver.com>
2022-07-25 21:55:09 +0900

    Implement forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,51 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "./api/utils";
+
 function App() {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [displayName, setDisplayName] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      axios
+        .get(`${BASE_URL}/v1/me`, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        })
+        .then((res) => setDisplayName(res.data.displayName))
+        .catch((err) => {
+          if (err.response.status === 401) {
+            localStorage.removeItem("token");
+            setToken(null);
+          }
+        });
+    }
+  }, [token]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+  };
+
   return (
     <div>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      Learn React
+      {token ? <h1>hello, {displayName}</h1> : <h1>simple react auth page</h1>}
+
+      <ol>
+        <li>
+          <button onClick={() => navigate("/login")}>login</button>
+        </li>
+        <li>
+          <button onClick={() => navigate("/register")}>register</button>
+        </li>
+        <li>
+          <button onClick={handleLogout}>logout</button>
+        </li>
+      </ol>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Login from "./views/Login";
+import Register from "./views/Register";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+    </Routes>
+  </BrowserRouter>
   // </React.StrictMode>
 );
